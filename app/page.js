aff883d404c36763cee8c88c8fb6a4a08514049b
Faisal Sayed <sayedfaisal544@gmail.com>
2024-12-11 20:02:01 +0530

    Add Todo List

--- app/page.js
+++ app/page.js
@@ -1,101 +1,69 @@
-import Image from "next/image";
-
+"use client";
+import { useState } from "react";
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [maintask, setMaintask] = useState([]);
+  function handleSubmit(e) {
+    e.preventDefault();
+    setMaintask([...maintask, { title, description }]);
+    setTitle("");
+    setDescription("");
+    console.log(maintask);
+  }
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  let renderList = (
+    <h1 className="text-xl font-bold text-center my-10 text-red-500">
+      NO TASK YET...
+    </h1>
+  );
+
+  renderList = maintask.map((taskItem, idx) => {
+    // console.log(`${taskItem.title} - ${taskItem.description}`);
+    return (
+      <div>
+        <div className="flex justify-between rounded">
+          <h2 className="p-2 m-2 w-1/3 bg-[#B2C9AD] font-bold text-xl font-serif rounded">
+            {taskItem.title}
+          </h2>
+          <h4 className="p-2 m-2 w-2/3 bg-[#91AC8F] font-mono rounded">
+            {taskItem.description}
+          </h4>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+      </div>
+    );
+  });
+  return (
+    <div className="m-2">
+      <h1 className="bg-slate-800 text-5xl text-[#00FF9C] text-center font-serif font-bold m-2 p-10 rounded">
+        Todo List
+      </h1>
+      <form className="m-2 rounded bg-slate-400 p-5" onSubmit={handleSubmit}>
+        <div className="flex justify-evenly">
+          <input
+            type="text"
+            placeholder="Task Title"
+            className="m-2 p-4 border-slate-500 text-xl border-4 rounded"
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+            value={title}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+          <input
+            type="text"
+            placeholder="Task Description"
+            className="m-2 p-4 border-slate-500 text-xl border-4 rounded"
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+            value={description}
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <button className="text-lg font-bold text-white bg-red-400 m-2 p-4 rounded">
+            Add Task
+          </button>
+        </div>
+      </form>
+      <div className="m-2">{renderList}</div>
     </div>
   );
 }
